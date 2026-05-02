13c437ef96965b6502d4602e36b3315527b43f58
Warren Parad <gpg@warrenparad.net>
2026-05-03 00:53:26 +0200

    Scaffolding.

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import './assets/main.css';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: { template: '<div />' } },
+  ],
+});
+
+createApp(App).use(router).mount('#app');
